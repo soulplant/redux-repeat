b1571e92f6ef03f524ddd1907c0914ed603e5c91
James Kozianski <james@helixta.com.au>
2017-10-15 07:56:06 +1100

    Implement the core idea.

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,148 @@
-import { add } from "./main";
+import { Dispatch, Store, applyMiddleware, createStore } from "redux";
+import { Effect, RouteBuilder, add, makeSagaMiddleware, reducer } from "./main";
 
-describe("things", () => {
-  it("works", () => {
-    expect(add(3, 5)).toBe(8);
+// Saga that waits in the "wait" state, then dispatches either onSuccess or
+// onError, depending on whether the thread was errored or not.
+const waitThenDispatch = <S>(onSuccess: string, onError: string) =>
+  function*(
+    dispatch: Dispatch<S>,
+    getState: () => S
+  ): IterableIterator<Effect> {
+    try {
+      const result = yield ["wait", null, null, null];
+      dispatch({ type: onSuccess });
+    } catch (e) {
+      dispatch({ type: onError });
+    }
+  };
+
+// Saga that waits in the "wait" state, then dispatches onTrue or onFalse,
+// depending on what "wait" yields.
+const ifTrueAElseB = <S>(onTrue: string, onFalse: string) =>
+  function*(
+    dispatch: Dispatch<S>,
+    getState: () => S
+  ): IterableIterator<Effect> {
+    const result = yield ["wait", null, null, null];
+    if (result) {
+      dispatch({ type: onTrue });
+    } else {
+      dispatch({ type: onFalse });
+    }
+  };
+
+// Consumes N messages in the "wait" state, then waits in the "end" state.
+const waitNTimes = <S>(n: number) =>
+  function*(
+    dispatch: Dispatch<S>,
+    getState: () => S
+  ): IterableIterator<Effect> {
+    for (let i = 0; i < n; i++) {
+      const result = yield ["wait", null, null, null];
+      dispatch({ type: "wait-" + i });
+    }
+    const result = yield ["end", null, null, null];
+    dispatch({ type: "done" });
+  };
+
+// Repeatedly blocks in the "wait" state until it yields true.
+const waitUntilTrue = function*<S>(
+  dispatch: Dispatch<S>,
+  getState: () => S
+): IterableIterator<Effect> {
+  while (true) {
+    const result = yield ["wait", null, null, null];
+    if (result) {
+      break;
+    }
+    dispatch({ type: "looping" });
+  }
+  dispatch({ type: "done" });
+};
+
+describe("simple-case", () => {
+  let routes: RouteBuilder;
+  let store: Store<String>;
+
+  beforeEach(() => {
+    routes = new RouteBuilder().takeEvery(
+      "A",
+      "A",
+      waitThenDispatch("SUCCESS", "ERROR")
+    );
+    store = createStore<string>(
+      reducer,
+      applyMiddleware(makeSagaMiddleware(routes))
+    );
+  });
+
+  it("starts in a consistent state", () => {
+    expect(store.getState()).toBe("@@redux/INIT");
+  });
+
+  it("continues", () => {
+    store.dispatch({ type: "A" });
+    expect(store.getState()).toBe("A");
+    routes.continueThread("A", "wait", 100);
+    expect(store.getState()).toBe("SUCCESS");
+  });
+
+  it("errors", () => {
+    store.dispatch({ type: "A" });
+    expect(store.getState()).toBe("A");
+    routes.errorThread("A", "wait", 100);
+    expect(store.getState()).toBe("ERROR");
+  });
+});
+
+describe("dynamic-looping-case", () => {
+  let routes: RouteBuilder;
+  let store: Store<String>;
+
+  beforeEach(() => {
+    routes = new RouteBuilder().takeEvery("A", "A", waitUntilTrue);
+    store = createStore<string>(
+      reducer,
+      applyMiddleware(makeSagaMiddleware(routes))
+    );
+  });
+
+  it("loops", () => {
+    store.dispatch({ type: "A" });
+    expect(store.getState()).toBe("A");
+    routes.continueThread("A", "wait", false);
+    expect(store.getState()).toBe("looping");
+    routes.continueThread("A", "wait", false);
+    expect(store.getState()).toBe("looping");
+    routes.continueThread("A", "wait", true);
+    expect(store.getState()).toBe("done");
+  });
+});
+
+describe("ping-pong", () => {
+  let routes: RouteBuilder;
+  let store: Store<String>;
+
+  beforeEach(() => {
+    // PING and PONG will start the other and terminate if they yield true, or
+    // dispatch DONE otherwise.
+    routes = new RouteBuilder()
+      .takeEvery("PING", "PING", ifTrueAElseB("PONG", "DONE"))
+      .takeEvery("PONG", "PONG", ifTrueAElseB("PING", "DONE"));
+    store = createStore<string>(
+      reducer,
+      applyMiddleware(makeSagaMiddleware(routes))
+    );
+  });
+
+  it("alternates", () => {
+    store.dispatch({ type: "PING" });
+    expect(store.getState()).toBe("PING");
+    routes.continueThread("PING", "wait", true);
+    expect(store.getState()).toBe("PONG");
+    routes.continueThread("PONG", "wait", true);
+    expect(store.getState()).toBe("PING");
+    routes.continueThread("PING", "wait", false);
+    expect(store.getState()).toBe("DONE");
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,174 @@
+import {
+  Action,
+  Dispatch,
+  Middleware,
+  MiddlewareAPI,
+  Store,
+  applyMiddleware,
+  createStore
+} from "redux";
+
+interface AsyncHandler {
+  handleAsync(
+    sagaName: string,
+    stepName: string,
+    object: any,
+    method: Function,
+    args: any[]
+  ): void;
+}
+
 export function add(x: number, y: number): number {
   return x + y;
 }
+
+export const reducer = (n: string = "", action: Action): string => {
+  return action.type;
+};
+
+class Clock {
+  delay(millis: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, millis);
+    });
+  }
+}
+
+const clock = new Clock();
+
+export type Effect = [string, any, any, any];
+
+function* saga(
+  clock: Clock,
+  dispatch: Dispatch<string>,
+  getState: () => string
+): IterableIterator<Effect> {
+  try {
+    console.log("about to wait");
+    const result = yield ["wait", clock, clock.delay, 100];
+    console.log("resuming with", result);
+    dispatch({ type: "A" });
+    yield ["wait-again", clock, clock.delay, 200];
+    dispatch({ type: "B" });
+  } catch (e) {
+    console.log("exception", e);
+    dispatch({ type: "EXCEPTION" });
+  }
+}
+
+type RouteEntry = {
+  type: string;
+  name: string;
+  saga(...args: any[]): Thread;
+  args: any[];
+};
+
+type Thread = IterableIterator<Effect>;
+
+type RunningThread = {
+  // The currently pending effect for this thread.
+  effect: Effect;
+
+  // The executing thread.
+  thread: Thread;
+};
+
+export class RouteBuilder {
+  private routes: { [type: string]: RouteEntry } = {};
+  private threads: { [type: string]: RunningThread } = {};
+  constructor() {}
+
+  // When an action with the given type is dispatched, start a fresh instance of
+  // the given saga. Multiple threads of the same saga are given an index equal
+  // to the number of concurrent threads in that saga already running.
+  takeEvery(
+    type: string,
+    name: string,
+    saga: (...args: any[]) => IterableIterator<Effect>,
+    ...args: any[]
+  ): RouteBuilder {
+    this.routes[type] = { type, saga, name, args };
+    return this;
+  }
+
+  // Called after an action is dispatched. This starts the relevant saga if
+  // necessary.
+  handleAction<S>(action: Action, store: MiddlewareAPI<S>): void {
+    const entry = this.routes[action.type];
+    if (!entry) {
+      return;
+    }
+    // TODO(james): When starting a thread deal with other threads for the same saga either by
+    // canceling them or ignoring them.
+    const thread = this.startThread(entry, store);
+    if (thread) {
+      this.threads[action.type] = thread;
+    }
+  }
+
+  continueThread(name: string, step: string, value: any): void {
+    const thread = this.getRunningThread(name, step);
+    const result = thread.thread.next(value);
+    if (result.done) {
+      this.removeCompletedThread(name, step);
+      return;
+    }
+    thread.effect = result.value;
+  }
+
+  errorThread(name: string, step: string, error: any): void {
+    const thread = this.getRunningThread(name, step);
+    thread.thread.throw!(error);
+  }
+
+  private removeCompletedThread(name: string, step: string): void {
+    // TODO(james): Ensure the step is right.
+    delete this.threads[name];
+  }
+
+  private getRunningThread(name: string, step: string): RunningThread {
+    const thread = this.threads[name];
+    if (step !== thread.effect[0]) {
+      throw new Error(
+        "Thread is in step " +
+          thread.effect[0] +
+          ", not " +
+          step +
+          ", so refusing to continue"
+      );
+    }
+    return thread;
+  }
+
+  startThread<S>(
+    entry: RouteEntry,
+    store: MiddlewareAPI<S>
+  ): RunningThread | null {
+    const thread = entry.saga(
+      ...entry.args,
+      store.dispatch.bind(store),
+      store.getState.bind(store)
+    );
+    const result = thread.next();
+    if (result.done) {
+      return null;
+    }
+
+    // TODO(james): If running in 'live' mode, execute the promise and hook it
+    // up to the corresponding thread.
+
+    return { thread, effect: result.value };
+  }
+}
+
+export const makeSagaMiddleware: (routes: RouteBuilder) => Middleware = (
+  routes: RouteBuilder
+) => <S>(store: MiddlewareAPI<S>) => <S>(next: Dispatch<S>): Dispatch<S> => <
+  A extends Action
+>(
+  action: A
+): A => {
+  next(action);
+  routes.handleAction(action, store);
+  return action;
+};
